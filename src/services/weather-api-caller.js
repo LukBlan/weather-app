5776926b8b0643a3d46cf0f3799cd05587fa9a62
Lukblan <luk.blanko@gmail.com>
2023-10-09 11:56:26 -0300

    Api caller now catch error results in api calls

---
+++ src/services/weather-api-caller.js
@@ -0,0 +1,19 @@
+import {subscribe} from "./pub-sub";
+
+subscribe("makeWeatherApiCall", makeWeatherApiCall);
+
+async function makeApiCall(cityName) {
+  const key = "0b3eed6dfdae45bcbad10452232309"
+  const url = `https://api.weatherapi.com/v1/current.json?KEY=${key}&q=${cityName}}`
+  const response = await fetch(url, {mode: 'cors'})
+
+  if (!response.ok) {
+    throw new Error("City Not Found")
+  }
+
+  return response.json();
+}
+
+function makeWeatherApiCall(cityName) {
+  makeApiCall(cityName).then(console.log).catch(() => {console.log("error")})
+}
--- src/view/search-city-view.js
+++ src/view/search-city-view.js
@@ -1,6 +1,6 @@
 import css from "./../styles.css";
 import {emit, subscribe} from "../services/pub-sub.js";
-import {} from "./../services/api-caller"
+import {} from "../services/weather-api-caller"
 
 const searchButton = document.querySelector(".search-button");
 const cityInput = document.getElementById("city-input");
@@ -18,6 +18,6 @@ function makeApiCallOnEnter(event) {
 function makeApiCall() {
   const cityName = cityInput.value;
   if (cityName.length !== 0) {
-    emit("makeApiCall", cityName)
+    emit("makeWeatherApiCall", cityName)
   }
 }
