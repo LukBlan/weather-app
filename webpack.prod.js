60952c16e3049787dc6e46f5fc6c08515fb19831
lukblan <luk.blanko@gmail.com>
2023-10-20 23:01:23 -0300

    Add css plugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -7,16 +10,30 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "main.js"
+    filename: "[name]-[contenthash].js"
+  },
+
+  optimization: {
+    minimize: true,
   },
 
   plugins: [
-    new HtmlWebpackPlugin({template: "./src/index-template.html"})
+    new HtmlWebpackPlugin({
+      template: "./src/index-template.html",
+      minify: {
+        removeComments: true,
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({filename: "[name]-[contenthash].css"}),
+    new CssMinimizerPlugin()
   ],
 
   module: {
     rules: [
-      {test: /\.css$/i, use: ["style-loader", "css-loader"],},
+      {test: /\.css$/i, use: [MiniCssExtractPlugin.loader, "css-loader"],},
     ],
   },
 
