e7ca0ad964cc1573eead0e1165182cfb83b93cbd
lukblan <luk.blanko@gmail.com>
2023-11-02 18:37:38 -0300

    Refactor temperature toggle

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import {makeApiCall} from "./components/search-city-bar/search-city.js";
 import {} from "./services/weather-api-caller.js"
 import {} from "./city/city-view.js"
 import {} from "./services/error-message-controller.js"
+import {} from "./components/temperature-toggle/temperature-toggle.js"
 import css from "./styles.css";
 
 // Start webpage displaying london info on screen
